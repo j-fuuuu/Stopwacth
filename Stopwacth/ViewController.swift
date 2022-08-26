291370339ebcd18b7e5f10d17418a3cea9d198cc
kumi fujii <kuum9393@gmail.com>
2022-08-26 17:43:45 +0900

    0826 変更済み

--- Stopwacth/ViewController.swift
+++ Stopwacth/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
     var count:Float = 0.0
     
     var timer: Timer = Timer()
+    
+    //判定ラベル
+    @IBOutlet var resultLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,15 +40,42 @@ class ViewController: UIViewController {
                                          repeats: true
             )
         }
+        resultLabel.text = ""
     }
     
     @IBAction func stop(){
         if timer.isValid{
             //タイマーが動作していたら停止する
             timer.invalidate()
+            
+        }
+        //判定のメソッドを呼び出し結果を表示
+        self.hantei()
+    }
+    
+    func hantei(){
+        //もし、経過時間が9.8~10.2秒の間だった場合Perfectと表示
+        if count > 9.8 && count < 10.20{
+            resultLabel.text = "Perfect!"
+        }else if count > 9.70 && count < 10.3{
+            resultLabel.text = "Great!" //経過時間が9.7から10.3秒だった場合GREATと表示
+        }else if count > 9.50 && count < 10.5{
+            resultLabel.text = "Good!"
+        }else{
+            resultLabel.text = "bad!"
         }
     }
-
-
+    
+    @IBAction func reset(){
+        if timer.isValid{
+            //タイマーが動いていたら止める
+            timer.invalidate()
+        }
+        //カウントを0にする
+        count = 0.0
+        //初期化したcountをLabelに表示
+        label.text = String(format: "%.2f", count)
+    }
+   
 }
 
